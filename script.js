faa79cae6cb2e02c01635838329af46cebcd0295
Pannu YN <pannuyannaing.fw@gmail.com>
2024-11-25 05:55:07 +0630

    design completed

---
+++ script.js
@@ -0,0 +1,9 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily: {
+          jakarta: ['"Plus Jakarta Sans"', 'sans-serif'],
+        },
+      },
+    },
+  };
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./index.html", // Add your HTML files here
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
